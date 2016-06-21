608a7907d157ec34b4e5a50b04220c6018706655
elena.balan <balan.helen@gmail.com>
2016-06-21 15:31:49 +0300

    modify a line

--- TestConsole/TestConsole/Program.cs
+++ TestConsole/TestConsole/Program.cs
@@ -11,6 +11,7 @@ namespace TestConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("sto nibudi");
         }
     }
 }
