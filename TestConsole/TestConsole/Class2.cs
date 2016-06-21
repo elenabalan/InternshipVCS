4cb5f53d020baeb1e2c2a56e456ab239524eed66
elena.balan <balan.helen@gmail.com>
2016-06-21 16:26:41 +0300

    Added Class2 with new fields

---
+++ TestConsole/TestConsole/Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Class2
+    {
+        int k;
+
+        public Class2()
+        {
+            k = 89;
+        }
+    }
+}
